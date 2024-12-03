733276055e9f118768d598206a663b0ddd0d29c6
Siriwipp <Siriwiphaauddee.mew@gmail.com>
2024-12-03 15:51:14 +0700

    lab7

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    float a  =  20, b = 30;
+    float c = 4;
+    float output; 
+    
+    output = a - b/c;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    cout << "output: " << output << "\n";
     return 0;
 }
