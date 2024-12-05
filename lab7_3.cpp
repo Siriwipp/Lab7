428a3075ba19e2ba909ec1bce4f04221b3e98bd6
Siriwipp <Siriwiphaauddee.mew@gmail.com>
2024-12-06 02:52:54 +0700

    lap7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	
+
+char before(char x) {
+
+    if (x >= 'A' && x <= 'Z') {
+      
+        if (x == 'A') {
+            return 'Z';
+        }
+        return x - 1;
+        
+    }else{
+     
+        return '0';
+    }
 }
 
 int main(){
